6c66b6a86c83180f4fb1e43f41f2dc6c7a8d9cf0
Bayram Utku Uzunlar <38724739+grandzero@users.noreply.github.com>
2021-01-16 22:18:53 +0300

    zIndex at griditem changed

--- src/components/ForeCastGrid/GridItem.js
+++ src/components/ForeCastGrid/GridItem.js
@@ -6,7 +6,7 @@ function GridItem({isLast,id,data}) {
     const dat = new Date();
     dat.setDate(new Date().getDate()+id+1)
     return (
-        <Card  loading={loading} bordered={false} style={{zIndex:-3,backgroundColor:"transparent"}} bodyStyle={{backgroundColor:"transparent",borderRight: !isLast && "1px solid white"}}>
+        <Card  loading={loading} bordered={false} style={{backgroundColor:"transparent"}} bodyStyle={{backgroundColor:"transparent",borderRight: !isLast && "1px solid white"}}>
             
                     <h2 style={{color:"white",textAlign:"center", margin:"auto"}}>{dat.toLocaleDateString("en-EN", { weekday: 'long' })}</h2>
                     <img alt="Weather" width="100%" height="100%" src={data.icon}></img>
